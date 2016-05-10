a86785f8b0ba3a82f37df53a2f499ce2c5557e84
Angad Singh <Angad Singh>
2016-05-10 16:13:57 -0400

    Socket io

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-
+var socket = require('socket.io');
 // hint: remember nodemon? i'd recommend it :)
 
 var PORT = 8080;
@@ -9,21 +9,28 @@ var PORT = 8080;
 var app = express();
 
 app.get('/twitter', function(req, res) {
-  res.sendFile(path.join(__dirname, '/../client/twitter.html'));
+    res.sendFile(path.join(__dirname, '/../client/twitter.html'));
 })
 
 app.get('/tweet', function(req, res) {
-  res.sendFile(path.join(__dirname, '/../client/tweet.html'));
+    res.sendFile(path.join(__dirname, '/../client/tweet.html'));
 })
 
-// hint: check out what body parser does
-
-// make a get('/tweets'), and post('/create_tweet') here
 
 //make the app listen on a port
-app.listen(PORT, function() {
-  console.log("The server is listening http://localhost:" + PORT);
+var server = app.listen(PORT, function() {
+    console.log("The server is listening http://localhost:" + PORT);
 })
+
+var io = socket.listen(server);
+
+io.on('connection', function(socket) {
+    console.log("A client has connected")
+    socket.on('get-tweets', function(data) {
+        //console.log(data);
+        socket.emit('tweets', {"Name": "Nick" })
+    })
+})// io connection
 
 /* to learn about how to get other require other files here
  * http://www.sitepoint.com/understanding-module-exports-exports-node-js/
